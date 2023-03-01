e8ad38cc3c579941dac45ebf4d619e3022eb4aa5
Hamza <hamzasahinofficial@gmail.com>
2023-03-01 15:16:30 +0100

    Tests Uploaded

--- src/Test/OrtakDosya.java
+++ src/Test/OrtakDosya.java
@@ -2,4 +2,5 @@ package Test;
 
 public class OrtakDosya {
     //Bunu main branch da iken oluşturdum ama add ve commit yapmadım.!!
+    //Test branch'ı local'den push ediliyor..
 }
---
+++ src/Test/YeniTest.java
@@ -0,0 +1,5 @@
+package Test;
+
+public class YeniTest {
+    //Local test Branch'ında eklendi..
+}
